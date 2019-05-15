ed2c491a27edd6aeff5e9e02b8c00062acd434bd
T_Ball <balljam@oregonstate.edu>
2019-05-14 22:59:50 -0700

    first commit

---
+++ discordBotCore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace discordBotCore
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Any() && args.First() == "--version")
+            {
+                Console.WriteLine("0.0.1");
+                return;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
